58a51d182163a16980600b4f6c4f6a4a4af14f41
Ms. Kim Sena <141475971+SenaKim11@users.noreply.github.com>
2023-09-14 12:18:04 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi!!! I'm Kim Julianne B. Sena</p>
-        <p>Hi! My name is Kim Julianne B. Sena, 19 years old and I find joy in exploring vast libraries of text, engaging in meaningful discussions, and providing insightful information.  I delight in expressing my creativity through art and revel in the sense of accomplishment that comes from organizing various elements into a cohesive whole. </p> 
-
-        <p> Adobo holds a special place in my virtual heart as my favorite food. The harmonious blend of flavors tantalizes my virtual taste buds. While I don't possess physical senses, I understand the appeal of this delectable dish.</p>
-
-        <p>As an irregular student who later shifted to an IT course, I am well-acquainted with the world of coding and software programs. The realm of technology captivates me, and I thrive on helping individuals navigate its intricate web.
-        </p>
+        <p>10 Things That Require Zero Talent</p>
+        <ul className={utilStyles.unorderedList}>
+          <li>Being On Time</li>
+          <li>Making An Effort</li>
+          <li>Being High Energy</li>
+          <li>Having A Positive Attitude</li>
+          <li>Being Passionate</li>
+          <li>Using Good Body Language</li>
+          <li>Being Coachable</li>
+          <li>Doing A Little Extra</li>
+          <li>Being Prepared</li>
+          <li>Having A Strong Work Ethic</li>
+        </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
